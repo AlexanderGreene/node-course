5025976b5e411db5780eca0c34c2a47f337cf012
Alex Greene <Alex.Greene@wendys.com>
2025-06-22 09:20:26 -0400

    use forecast node api to put forecast on website

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,9 +6,27 @@ fetch('https://puzzle.mead.io/puzzle').then((response) => {
 	});
 });
 
-fetch('http://localhost:3000/weather?address=columbus').then((response) => {
-	response.json().then((data) => {
-		if (data.error) return console.log(data.error);
-		console.log(data);
-	});
+const weatherForm = document.querySelector('form');
+const searchEle = document.querySelector('input');
+const message = document.querySelector('#message');
+
+weatherForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const location = searchEle.value;
+	fetch('http://localhost:3000/weather?address=' + location).then(
+		(response) => {
+			response.json().then((data) => {
+				if (data.error) return (message.textContent = data.error);
+				message.textContent =
+					data.location +
+					'\nIt is currently ' +
+					data.description +
+					' with a temperature of ' +
+					data.temperature +
+					', but it feels like ' +
+					data.feelslike +
+					'.';
+			});
+		}
+	);
 });
