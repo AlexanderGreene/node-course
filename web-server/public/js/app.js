d5be45ca18cf358d8302e6cb1c7021530f66ca77
Alex Greene <Alex.Greene@wendys.com>
2025-06-23 13:27:03 -0400

    set up repo to be used on heroku remove some unused code

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,3 @@
-console.log('Client side javascript file is loaded!');
-
-fetch('https://puzzle.mead.io/puzzle').then((response) => {
-	response.json().then((data) => {
-		console.log(data);
-	});
-});
-
 const weatherForm = document.querySelector('form');
 const searchEle = document.querySelector('input');
 const message = document.querySelector('#message');
@@ -14,20 +6,18 @@ weatherForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	message.textContent = 'Loading...';
 	const location = searchEle.value;
-	fetch('http://localhost:3000/weather?address=' + location).then(
-		(response) => {
-			response.json().then((data) => {
-				if (data.error) return (message.textContent = data.error);
-				message.textContent =
-					data.location +
-					'\nIt is currently ' +
-					data.description +
-					' with a temperature of ' +
-					data.temperature +
-					', but it feels like ' +
-					data.feelslike +
-					'.';
-			});
-		}
-	);
+	fetch('/weather?address=' + location).then((response) => {
+		response.json().then((data) => {
+			if (data.error) return (message.textContent = data.error);
+			message.textContent =
+				data.location +
+				'\nIt is currently ' +
+				data.description +
+				' with a temperature of ' +
+				data.temperature +
+				', but it feels like ' +
+				data.feelslike +
+				'.';
+		});
+	});
 });
