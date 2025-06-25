abc8d18d3e29655459b391aa42dbd650e21c4128
Alex Greene <Alex.Greene@wendys.com>
2025-06-25 16:37:55 -0400

    refactor task model set up new task post endpoint

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -8,18 +8,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 	// useUnifiedTopology: true,
 });
 
-const Task = mongoose.model('Task', {
-	description: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	completed: {
-		type: Boolean,
-		default: false,
-	},
-});
-
 // uncomment this and switch the require/import statements to view collections without Compass
 // const usersQuery = await User.find().exec();
 // const tasksQuery = await Task.find().exec();
