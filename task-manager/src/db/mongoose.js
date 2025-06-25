99e00816c004aae6e542d644cc71603baf97fa1f
Alex Greene <Alex.Greene@wendys.com>
2025-06-25 16:23:31 -0400

    refactor code to have models in their own directory introduce express pare down mongoose.js to only have logic pertaining to db functions

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,53 +1,11 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 // import mongoose from 'mongoose';
-// import validator from 'validator';
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 	// deprecated connection options, but were used in the class. leaving for posterity
 	// useNewUrlParser: true,
 	// useCreateIndex: true,
 	// useUnifiedTopology: true,
-});
-
-const User = mongoose.model('User', {
-	name: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		trim: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error('Email is invalid.');
-			}
-		},
-	},
-	age: {
-		type: Number,
-		default: 0,
-		validate(value) {
-			if (value < 0) {
-				throw new Error('Age must not be negative.');
-			}
-		},
-	},
-	password: {
-		type: String,
-		required: true,
-		minlength: 7,
-		trim: true,
-		validate(value) {
-			if (value.toLowerCase().includes('password'))
-				throw new Error(
-					'Come on bud...You can think of a better password than that.'
-				);
-		},
-	},
 });
 
 const Task = mongoose.model('Task', {
@@ -62,18 +20,6 @@ const Task = mongoose.model('Task', {
 	},
 });
 
-const me = new User({
-	name: 'Devon',
-	email: 'notreallydevon@email.com',
-	age: 44,
-	password: 'nicePaSsWoRd',
-});
-
-const it = new Task({
-	description: 'Do it! Seriously!',
-	completed: false,
-});
-
 // uncomment this and switch the require/import statements to view collections without Compass
 // const usersQuery = await User.find().exec();
 // const tasksQuery = await Task.find().exec();
@@ -83,6 +29,17 @@ const it = new Task({
 
 // Commented out so it doesn't create duplicate records
 // Uncomment to populate new instance of db
+// const me = new User({
+// 	name: 'Devon',
+// 	email: 'notreallydevon@email.com',
+// 	age: 44,
+// 	password: 'nicePaSsWoRd',
+// });
+
+// const it = new Task({
+// 	description: 'Do it! Seriously!',
+// 	completed: false,
+// });
 // me.save()
 // 	.then(() => {
 // 		console.log(me);
