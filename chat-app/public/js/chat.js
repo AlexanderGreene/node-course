215ede4abbb9a3036aaff3c68405280a16c5a206
Alex <greene.alexanderj@gmail.com>
2025-07-11 07:24:57 -0400

    i forget but i know we did something

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,58 @@
-io();
+const socket = io();
+
+// server (emit) -> client (receive) --acknowledgement -> server
+// client (emit) -> server (receive) --acknowledgement -> client
+
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+
+socket.on('message', (message) => {
+	console.log(message);
+});
+
+$messageForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	$messageFormButton.setAttribute('disabled', 'disabled');
+
+	const message = e.target.elements.message.value;
+
+	socket.emit('sendMessage', message, (error) => {
+		$messageFormButton.removeAttribute('disabled');
+		$messageFormInput.value = '';
+		$messageFormInput.focus();
+
+		if (error) return console.error(error);
+
+		console.log('Message delivered!');
+	});
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+	if (!navigator.geolocation)
+		return alert('Geolocation is not supported by your browser');
+
+	navigator.geolocation.getCurrentPosition((position) => {
+		socket.emit(
+			'sendLocation',
+			{
+				lat: position.coords.latitude,
+				long: position.coords.longitude,
+			},
+			() => {
+				console.log('Location sent!');
+			}
+		);
+	});
+});
+
+// Counter example
+// socket.on('countUpdated', (count) => {
+// 	console.log('The count has been updated', count);
+// });
+
+// document.querySelector('#increment').addEventListener('click', () => {
+// 	console.log('Clicked');
+// 	socket.emit('increment');
+// });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 const serverRunning = require('./serverRunning');
 
 const app = express();
@@ -14,8 +15,46 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+// let count = 0;
+// server (emit) --> client (receive) - countUpdated
+// client (emit) --> server (receive) - increment
+
+io.on('connection', (socket) => {
 	console.log('New WebSocket connection');
+
+	socket.emit('message', 'Welcome!');
+	socket.broadcast.emit('message', 'A new user has joined!');
+
+	socket.on('sendMessage', (message, callback) => {
+		const filter = new Filter();
+
+		if (filter.isProfane(message)) {
+			return callback('No fucking profanity, dipshit!');
+		}
+
+		io.emit('message', message);
+		callback();
+	});
+
+	socket.on('sendLocation', (coords, callback) => {
+		io.emit(
+			'message',
+			`Location: https://google.com/maps?q=${coords.lat},${coords.long}`
+		);
+		callback();
+	});
+
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user has left :(');
+	});
+	// Counter example
+	// socket.emit('countUpdated', count);
+
+	// socket.on('increment', () => {
+	// 	count++;
+	// 	// socket.emit('countUpdated', count); --emit to just single particular connection
+	// 	io.emit('countUpdated', count); // emit to all connections
+	// });
 });
 
 // app.com
